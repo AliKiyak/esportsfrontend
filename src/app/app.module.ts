a9e64fbb2eb232c9dc71a2ee0f81abd2aeeae095
AliKiyak <r0702374@student.thomasmore.be>
2019-11-03 14:17:06 +0100

    added addgame and filtergame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TeamComponent } from './team/team.component';
+import { DetailGameComponent } from './game/detail-game/detail-game.component';
+import { AddGameComponent } from './game/add-game/add-game.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 const appRoutes = [
-  {path: '', component: GameComponent}
+  {path: 'games', component: GameComponent},
+  {path: 'games/detail/:gameid', component: DetailGameComponent},
+  {path: 'games/addgame', component: AddGameComponent},
+  {path: 'teams', component: TeamComponent},
+  {path: 'teams/:gameid', component: TeamComponent},
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    TeamComponent,
+    DetailGameComponent,
+    AddGameComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { enableTracing: false }),
     HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
