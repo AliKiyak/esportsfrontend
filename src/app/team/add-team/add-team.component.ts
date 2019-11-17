2d899f01d453ab6ba91a71db6c3a5e933fe1eb0e
AliKiyak <r0702374@student.thomasmore.be>
2019-11-17 10:17:35 +0100

    added image to addteam

--- src/app/team/add-team/add-team.component.ts
+++ src/app/team/add-team/add-team.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TeamService } from '../team.service';
 import { GameService} from '../../game/game.service';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-add-team',
@@ -19,16 +20,35 @@ export class AddTeamComponent implements OnInit {
     imageUrl: new FormControl(''),
     owner: new FormControl(''),
     description: new FormControl(''),
-
   });
-  constructor(private router: Router, private teamService: TeamService, private gameService: GameService) { }
+  profilePicture: File = null;
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+  private basePath = '/teams';
+  url: any;
+  constructor(private router: Router, private teamService: TeamService, private gameService: GameService,
+              private afStorage: AngularFireStorage) { }
 
   ngOnInit() {
     this.gameService.getGames().subscribe(
       result => this.games = result
     );
   }
-  onSubmit() {
+  async onPictureChange(event) {
+    this.profilePicture = <File>event.target.files[0];
+  }
+
+  async onSubmit() {
+    const filePath = `${this.basePath}/${this.profilePicture.name}`;
+
+    // uploads the image into the Firebase storage server
+    const task = await this.afStorage.upload(filePath, this.profilePicture);
+
+    // gets back the uploaded image in a URL
+    this.url =  await task.ref.getDownloadURL();
+
+    this.teamForm.controls['imageUrl'].setValue(this.url);
+
     this.teamService.addTeam(this.teamForm.value).subscribe(
       result => { console.log(result); this.router.navigate(['/teams']); }
     );
